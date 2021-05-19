a3df5a95727b77268aa9df924d4e03b7585c086e
Yasuhiro Ishii <73149353+yasu971@users.noreply.github.com>
2021-05-20 08:35:49 +0900

    JavaScript2

---
+++ JavaScript2/index.js
@@ -0,0 +1,32 @@
+window.addEventListener('load', function(){
+
+  const pullDownButton = document.getElementById("lists")
+  const pullDownParents = document.getElementById("pull-down")
+  const pullDownChild = document.querySelectorAll(".pull-down-list")
+  const currentList = document.getElementById("current-list")
+
+  pullDownButton.addEventListener('mouseover', function(){
+    this.setAttribute("style", "background-color:#FFBEDA;")
+  })  
+
+  pullDownButton.addEventListener('mouseout', function(){
+    this.removeAttribute("style", "background-color:#FFBEDA;")
+  })
+
+  pullDownButton.addEventListener('click', function() {
+    // プルダウンメニューの表示と非表示の設定
+    if (pullDownParents.getAttribute("style") == "display:block;") {
+      pullDownParents.removeAttribute("style", "display:block;")
+    } else {
+      pullDownParents.setAttribute("style", "display:block;")
+    }
+  })
+
+  // コースの値を取得し表示する
+  pullDownChild.forEach(function(list) {
+    list.addEventListener('click', function() {
+      value = list.innerHTML
+      currentList.innerHTML = value
+    })    
+  })  
+})
