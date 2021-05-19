dcf6337dc3e0f11317e79e0b3b4a3ef5aa1ee0a4
Yasuhiro Ishii <73149353+yasu971@users.noreply.github.com>
2021-05-20 08:35:02 +0900

    JavaScript1

---
+++ JavaScript1/index.js
@@ -0,0 +1,32 @@
+function pullDown() {
+
+  const pullDownButton = document.getElementById("lists")
+  const pullDownParents = document.getElementById("pull-down")
+  const pullDownChild = document.querySelectorAll(".pull-down-list")
+  const currentList = document.getElementById("current-list")
+
+  pullDownButton.addEventListener('mouseover', function(){
+    this.setAttribute("style", "background-color:#FFBEDA;")
+  })
+
+  pullDownButton.addEventListener('mouseout', function(){
+    this.removeAttribute("style", "background-color:#FFBEDA;")
+  })
+
+  pullDownButton.addEventListener('click', function() {
+    if (pullDownParents.getAttribute("style") == "display:block;") {
+      pullDownParents.removeAttribute("style", "display:block;")
+    } else {
+      pullDownParents.setAttribute("style", "display:block;")
+    }
+  })
+
+  pullDownChild.forEach(function(list) {
+    list.addEventListener('click', function() {
+      value = list.innerHTML
+      currentList.innerHTML = value
+    })
+  })
+}
+
+window.addEventListener('load', pullDown)
