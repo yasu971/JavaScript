9f2a8fb02e2caf56cc953daa12cecb0539dd31cb
Yasuhiro Ishii <73149353+yasu971@users.noreply.github.com>
2021-05-21 10:35:38 +0900

    Index.js-update4
    ・JavaScriptを編集して、クリック時にプルダウンメニューの表示・非表示を切り替える。

--- JavaScript3/index.js
+++ JavaScript3/index.js
@@ -2,16 +2,23 @@ window.addEventListener('load', function(){
 
   const pullDownButton = document.getElementById("lists")
   const pullDownParents = document.getElementById("pull-down")
-
-  pullDownButton.addEventListener('mouseover', function(){
-    this.setAttribute("style", "background-color:#FFBEDA;")
+  
+    pullDownButton.addEventListener('mouseover', function(){
+      this.setAttribute("style", "background-color:#FFBEDA;")
+    })
+  
+    pullDownButton.addEventListener('mouseout', function(){
+      this.removeAttribute("style", "background-color:#FFBEDA;")
+    })
+  
+    pullDownButton.addEventListener('click', function() {
+      // プルダウンメニューの表示と非表示の設定
+      if (pullDownParents.getAttribute("style") == "display:block;") {
+        // pullDownParentsにdisplay:block;が付与されている場合（つまり表示されている時）実行される
+        pullDownParents.removeAttribute("style", "display:block;")
+      } else {
+        // pullDownParentsにdisplay:block;が付与されていない場合（つまり非表示の時）実行される
+        pullDownParents.setAttribute("style", "display:block;")
+      }
+    })
   })
-
-  pullDownButton.addEventListener('mouseout', function(){
-    this.removeAttribute("style", "background-color:#FFBEDA;")
-  })
-
-  pullDownButton.addEventListener('click', function() {
-    pullDownParents.setAttribute("style", "display:block;")
-  })
-})
