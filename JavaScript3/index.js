424382b998a98cae4e079c4501d67146596790c0
Yasuhiro Ishii <73149353+yasu971@users.noreply.github.com>
2021-05-21 10:38:06 +0900

    Index.js-update5
    ・プルダウンメニューが表示される挙動をデバッグする。

--- JavaScript3/index.js
+++ JavaScript3/index.js
@@ -12,6 +12,7 @@ window.addEventListener('load', function(){
     })
   
     pullDownButton.addEventListener('click', function() {
+      debugger
       // プルダウンメニューの表示と非表示の設定
       if (pullDownParents.getAttribute("style") == "display:block;") {
         // pullDownParentsにdisplay:block;が付与されている場合（つまり表示されている時）実行される
