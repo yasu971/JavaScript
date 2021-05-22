a4bf4c28c4d63a4b445b6d54fc3c5ce0bace8e64
Yasuhiro Ishii <73149353+yasu971@users.noreply.github.com>
2021-05-22 11:46:37 +0900

    Index.js-update8
    clickイベントを指定。

--- JavaScript3/index.js
+++ JavaScript3/index.js
@@ -3,23 +3,28 @@ window.addEventListener('load', function(){
   const pullDownButton = document.getElementById("lists")
   const pullDownParents = document.getElementById("pull-down")
   const pullDownChild = document.querySelectorAll(".pull-down-list")
-  
-    pullDownButton.addEventListener('mouseover', function(){
-      this.setAttribute("style", "background-color:#FFBEDA;")
-    })
-  
-    pullDownButton.addEventListener('mouseout', function(){
-      this.removeAttribute("style", "background-color:#FFBEDA;")
-    })
-  
-    pullDownButton.addEventListener('click', function() {
-      // プルダウンメニューの表示と非表示の設定
-      if (pullDownParents.getAttribute("style") == "display:block;") {
-        // pullDownParentsにdisplay:block;が付与されている場合（つまり表示されている時）実行される
-        pullDownParents.removeAttribute("style", "display:block;")
-      } else {
-        // pullDownParentsにdisplay:block;が付与されていない場合（つまり非表示の時）実行される
-        pullDownParents.setAttribute("style", "display:block;")
-      }
+
+  pullDownButton.addEventListener('mouseover', function(){
+    this.setAttribute("style", "background-color:#FFBEDA;")
+  })
+
+  pullDownButton.addEventListener('mouseout', function(){
+    this.removeAttribute("style", "background-color:#FFBEDA;")
+  })
+
+  pullDownButton.addEventListener('click', function() {
+    // プルダウンメニューの表示と非表示の設定
+    if (pullDownParents.getAttribute("style") == "display:block;") {
+      pullDownParents.removeAttribute("style", "display:block;")
+    } else {
+      pullDownParents.setAttribute("style", "display:block;")
+    }
+  })
+
+  // コースの値を取得し表示する
+  pullDownChild.forEach(function(list) {
+    list.addEventListener('click', function() {
+      console.log(list)
     })
   })
+})
