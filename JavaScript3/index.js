844624fbf7d07070ae0ccefd5ce26984a9b3c7c0
Yasuhiro Ishii <73149353+yasu971@users.noreply.github.com>
2021-05-23 10:11:50 +0900

    index.js-update10
    ・JavaScriptを編集して、今回定義したcurrentListを用いて、変数valueの値をブラウザ上に表示。

--- JavaScript3/index.js
+++ JavaScript3/index.js
@@ -3,7 +3,8 @@ window.addEventListener('load', function(){
   const pullDownButton = document.getElementById("lists")
   const pullDownParents = document.getElementById("pull-down")
   const pullDownChild = document.querySelectorAll(".pull-down-list")
-
+  const currentList = document.getElementById("current-list")
+  
   pullDownButton.addEventListener('mouseover', function(){
     this.setAttribute("style", "background-color:#FFBEDA;")
   })
