cf3654f5e1945096a9d25c9c4088df7e3c5ea42b
Yasuhiro Ishii <73149353+yasu971@users.noreply.github.com>
2021-05-23 10:07:12 +0900

    index.js-update9
    ・JavaScriptを編集して、クリックした要素の文字列を取得。

--- JavaScript3/index.js
+++ JavaScript3/index.js
@@ -24,7 +24,8 @@ window.addEventListener('load', function(){
   // コースの値を取得し表示する
   pullDownChild.forEach(function(list) {
     list.addEventListener('click', function() {
-      console.log(list)
+      value = list.innerHTML
+      console.log(value)
     })
   })
 })
