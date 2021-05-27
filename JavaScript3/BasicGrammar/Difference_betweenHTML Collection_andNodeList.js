1966422c7a01e99f959f49da7bd6f20e3cc6ad1f
Yasuhiro Ishii <73149353+yasu971@users.noreply.github.com>
2021-05-27 13:20:38 +0900

    Difference_betweenHTML Collection_andNodeList.js
    【例】HTMLCollectionとNodeListの違い

---
+++ JavaScript3/BasicGrammar/Difference_betweenHTML Collection_andNodeList.js
@@ -0,0 +1,5 @@
+console.log(document.getElementsByClassName("modal-list"))
+// => HTMLCollection(4) [li.modal-list, li.modal-list, li.modal-list, li.modal-list]
+
+console.log(document.querySelectorAll(".modal-list"))
+// => NodeList(4) [li.modal-list, li.modal-list, li.modal-list, li.modal-list]
