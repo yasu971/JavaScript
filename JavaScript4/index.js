28830ad28e199bc3941d172f6cc75d978421dbc1
Yasuhiro Ishii <73149353+yasu971@users.noreply.github.com>
2021-05-31 10:57:34 +0900

    index.js-update4
    マウスカーソルが要素の上に乗った時にイベント発火させる。

--- JavaScript4/index.js
+++ JavaScript4/index.js
@@ -1,4 +1,8 @@
 window.addEventListener('load', function(){
 
   const pullDownButton = document.getElementById("lists")
+
+  pullDownButton.addEventListener('mouseover', function(){
+    console.log("mouseover OK")
+  })
 })
