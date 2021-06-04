04122b2485a8c462333798cdfe495440213b9853
Yasuhiro Ishii <73149353+yasu971@users.noreply.github.com>
2021-06-04 09:21:48 +0900

    index.js-update14
    JavaScriptを用いて、クリック時にプルダウンメニューの表示・非表示を切り替え。

--- JavaScript4/index.js
+++ JavaScript4/index.js
@@ -13,6 +13,13 @@ window.addEventListener('load', function(){
 
   
   pullDownButton.addEventListener('click', function() {
-    pullDownParents.setAttribute("style", "display:block;")
+    // プルダウンメニューの表示と非表示の設定
+    if (pullDownParents.getAttribute("style") == "display:block;") {
+      // pullDownParentsにdisplay:block;が付与されている場合（つまり表示されている時）実行される
+      pullDownParents.removeAttribute("style", "display:block;")
+    } else {
+      // pullDownParentsにdisplay:block;が付与されていない場合（つまり非表示の時）実行される
+      pullDownParents.setAttribute("style", "display:block;")
+    }    
   })
 })
