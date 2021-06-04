90fd300ed379389cf5a2f929ea68507d10918b44
Yasuhiro Ishii <73149353+yasu971@users.noreply.github.com>
2021-06-04 09:17:28 +0900

    index.js-update13
    thisを使って書き換え2。

--- JavaScript4/index.js
+++ JavaScript4/index.js
@@ -8,7 +8,7 @@ window.addEventListener('load', function(){
   })
 
   pullDownButton.addEventListener('mouseout', function(){
-    pullDownButton.removeAttribute("style", "background-color:#FFBEDA;")
+    this.removeAttribute("style", "background-color:#FFBEDA;")
   })
 
   
