c322873867bfd42bb1dfb0ab434aa4b0abe97305
Yasuhiro Ishii <73149353+yasu971@users.noreply.github.com>
2021-06-05 13:42:57 +0900

    index.js-update16
    pullDownChildに格納されているHTML要素の1つひとつに対して処理を行うことが可能となります。

--- JavaScript4/index.js
+++ JavaScript4/index.js
@@ -12,15 +12,16 @@ window.addEventListener('load', function(){
     this.removeAttribute("style", "background-color:#FFBEDA;")
   })
 
-  
   pullDownButton.addEventListener('click', function() {
     // プルダウンメニューの表示と非表示の設定
     if (pullDownParents.getAttribute("style") == "display:block;") {
-      // pullDownParentsにdisplay:block;が付与されている場合（つまり表示されている時）実行される
       pullDownParents.removeAttribute("style", "display:block;")
     } else {
-      // pullDownParentsにdisplay:block;が付与されていない場合（つまり非表示の時）実行される
       pullDownParents.setAttribute("style", "display:block;")
-    }    
+    }
+  })
+
+  // コースの値を取得し表示する
+  pullDownChild.forEach(function(list) {
   })
 })
