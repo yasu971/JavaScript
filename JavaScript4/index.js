1f4d71d85e3e3b15785479e7522a77486608ee87
Yasuhiro Ishii <73149353+yasu971@users.noreply.github.com>
2021-06-06 16:01:15 +0900

    index.js-update17
    forEachで取得した要素に対して、イベントを仕込んでおきます。

--- JavaScript4/index.js
+++ JavaScript4/index.js
@@ -23,5 +23,8 @@ window.addEventListener('load', function(){
 
   // コースの値を取得し表示する
   pullDownChild.forEach(function(list) {
+    list.addEventListener('click', function() {
+      console.log(list)
+    })    
   })
 })
