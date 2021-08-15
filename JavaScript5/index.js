8225c7ba6ea57480107675b3a9cd36486ade869f
Yasuhiro Ishii <73149353+yasu971@users.noreply.github.com>
2021-08-15 10:51:38 +0900

    index.js-update04
    マウスカーソルが要素から外れた時にイベント発火。

--- JavaScript5/index.js
+++ JavaScript5/index.js
@@ -1,8 +1,12 @@
 window.addEventListener('load', function(){
-
   const pullDownButton = document.getElementById("lists")
 
   pullDownButton.addEventListener('mouseover', function(){
     console.log("mouseover OK")
   })
+
+  pullDownButton.addEventListener('mouseout', function(){
+    console.log("mouseout OK")
+  })
+
 })
