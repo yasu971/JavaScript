5e681b77e8e868f8f20bdf9d66056b4865de8830
Yasuhiro Ishii <73149353+yasu971@users.noreply.github.com>
2022-08-25 16:11:31 +0900

    Update01_sample.js

--- JavaScrip6/sample.js
+++ JavaScrip6/sample.js
@@ -1,7 +1,5 @@
-let sample = "おはよう"
-
-sample = "おはよう"
-// 再代入OK
-
-let sample = "おはよう"
-// 再定義NG →エラーが起こる
+let count = 1
+for (let i = 1; i <= 10; i += 1) {
+  console.log(`${count}回目の出力`)
+  count +=  1
+}
