931ad9629bc26a5322fa977ec0adb190a62eae2a
Yasuhiro Ishii <73149353+yasu971@users.noreply.github.com>
2022-09-04 11:35:43 +0900

    Update01_index.js

--- JavaScrip6/index.js
+++ JavaScrip6/index.js
@@ -1,34 +1,32 @@
-<!DOCTYPE html>
-<html>
-<head>
-  <link rel="stylesheet" href="index.css">
-  <script src="index.js"></script>
-  <meta charset="UTF-8">
-  <title>JavaScriptの練習サイト</title>
-</head>
-<body>
-  <header>
-    <div class="heading">
-      <h1 class="title">TECH HEADER</h1>
-    </div>
-    <div class="navbar">
-      <ul class="nav-lists">
-        <li class="list-status">
-          現在のリスト；
-          <span id="current-list">選択されていません</span>
-        </li>
-        <li class="background-red" id="lists">リスト
-        </li>
-        <li>カリキュラム変更</li>
-      </ul>
-      <ul class="show-lists hidden" id="pull-down">
-        <li class="pull-down-list">リスト1</li>
-        <li class="pull-down-list">リスト2</li>
-        <li class="pull-down-list">リスト3</li>
-        <li class="pull-down-list">リスト4</li>
-      </ul>
-    </div>
-  </header>
-  <div class="contents"></div>
-</body>
-</html>
+window.addEventListener('load', function(){
+
+    const pullDownButton = document.getElementById("lists")
+    const pullDownParents = document.getElementById("pull-down")
+    const pullDownChild = document.querySelectorAll(".pull-down-list")
+    const currentList = document.getElementById("current-list")
+  
+    pullDownButton.addEventListener('mouseover', function(){
+      this.setAttribute("style", "background-color:#FFBEDA;")
+    })  
+  
+    pullDownButton.addEventListener('mouseout', function(){
+      this.removeAttribute("style", "background-color:#FFBEDA;")
+    })
+  
+    pullDownButton.addEventListener('click', function() {
+      // プルダウンメニューの表示と非表示の設定
+      if (pullDownParents.getAttribute("style") == "display:block;") {
+        pullDownParents.removeAttribute("style", "display:block;")
+      } else {
+        pullDownParents.setAttribute("style", "display:block;")
+      }
+    })
+  
+    // コースの値を取得し表示する
+    pullDownChild.forEach(function(list) {
+      list.addEventListener('click', function() {
+        value = list.innerHTML
+        currentList.innerHTML = value
+      })    
+    })  
+  })  
