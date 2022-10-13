1d18018574c5bc2091afaf34241394e42882ef68
Yasuhiro Ishii <73149353+yasu971@users.noreply.github.com>
2022-10-13 12:59:17 +0900

    property_object1.js

---
+++ JavaScript7/property_object1.js
@@ -0,0 +1,13 @@
+let human = {
+    name: "yamada",
+    age: 25,
+    address: 'Tokyo'
+  }
+  
+  console.log(human.name)
+  
+  human.name = "yabe"
+  console.log(human.name)
+  
+  //  Googleﾃﾞｨﾍﾟﾛｯﾊﾟﾂｰﾙで、実行してください。
+Footer
