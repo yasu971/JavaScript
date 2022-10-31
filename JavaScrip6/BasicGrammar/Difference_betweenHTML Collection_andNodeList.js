d37a26f29a74e6ab747096f2de94ad46196926aa
Yasuhiro Ishii <73149353+yasu971@users.noreply.github.com>
2022-10-31 12:49:15 +0900

    Update01

--- JavaScrip6/BasicGrammar/Difference_betweenHTML Collection_andNodeList.js
+++ JavaScrip6/BasicGrammar/Difference_betweenHTML Collection_andNodeList.js
@@ -3,4 +3,3 @@ console.log(document.getElementsByClassName("modal-list"))
 
 console.log(document.querySelectorAll(".modal-list"))
 // => NodeList(4) [li.modal-list, li.modal-list, li.modal-list, li.modal-list]
-Footer
