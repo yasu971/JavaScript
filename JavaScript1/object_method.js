c92ab3397baaa93900ca5313f068f34e2bf1f527
Yasuhiro Ishii <73149353+yasu971@users.noreply.github.com>
2022-11-24 12:56:03 +0900

    object_method.js

---
+++ JavaScript1/object_method.js
@@ -0,0 +1,18 @@
+let human = {
+    name: "Tom",
+    gender: man,
+    age: 22,
+  
+    walk: function(){...},
+    talk: function(){...},
+    eat: function(){...}
+  }
+  
+  human.walk()
+  // walkメソッドが実行される
+  
+  human.talk()
+  // talkメソッドが実行される
+  
+  human.eat()
+  // eatメソッドが実行される
